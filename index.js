dd7e958e1c6a8c716f06bafd0ffbdb8570b2236a
Focus2334 <danilakurzuev@gmail.com>
2026-04-07 19:53:23 +0500

    add progress

--- index.js
+++ index.js
@@ -5,3 +5,24 @@
     element.style.color = 'red';
     element.style.width = '300px';
 */
+
+const progressBar = document.querySelector('.progress-bar');
+
+function setProgress(value) {
+    value = Math.max(0, Math.min(100, value));
+
+    progressBar.style.setProperty('--progress', value + '%');
+
+    progressBar.setAttribute('data-text', `Loading... ${value}%`);
+}
+let progress = 0;
+
+const interval = setInterval(() => {
+    progress += 5;
+    setProgress(progress);
+
+    if (progress >= 100) {
+        clearInterval(interval);
+        progressBar.setAttribute('data-text', 'Done ✅');
+    }
+}, 300);
